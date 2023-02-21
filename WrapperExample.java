56f53bd351090ceb38b95b9ce4d0cedc2144ae2a
Rohuu <rohuu99022@gmail.com>
2023-02-21 20:02:01 +0530

    intializing variable using wrapper class

---
+++ WrapperExample.java
@@ -0,0 +1,18 @@
+public class WrapperExample {
+    public static void main(String[] args) {
+        int a = 10;
+        // a. will not give many options but num. will give us many options since it is
+        // an object of class Integer.
+        // or we can do this by making object of Integer class as well
+        Integer num = 45;
+        // this is now created as an object.
+
+        //////// final keyword
+        final int increase = 1;
+        increase = 3;
+        // this will give error because increase variable is final type and we cant
+        // change final values.
+        // and final variables have to be initialized.
+
+    }
+}
