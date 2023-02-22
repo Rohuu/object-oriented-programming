0ea683c1d50f8a4a1ec430d806453fe6e6c537af
Rohuu <rohuu99022@gmail.com>
2023-02-22 23:50:30 +0530

    accessing method from other class in other package

---
+++ package1/Greeting.java
@@ -0,0 +1,10 @@
+package package1;
+
+public class Greeting {
+    public static void main(String[] args) {
+
+    }
+    void greeting(){
+        System.out.println("hi, i am in package1 right now");
+    }
+}
---
+++ package2/Message.java
@@ -0,0 +1,11 @@
+package package2;
+import static package1.Message.greeting;
+public class Message {
+    public static void main(String[] args) {
+        System.out.println("this is pakcage2");
+        greeting();
+
+        // like this we can call method from other class in other package also
+        //static import packcge name.class.method;
+    }
+}
