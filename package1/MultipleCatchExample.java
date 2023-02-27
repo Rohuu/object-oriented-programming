bf880ea46b5ef93a2d31a70ec3c1ea1d898e6a95
Rohuu <rohuu99022@gmail.com>
2023-02-27 19:57:47 +0530

    multiple catch for a try

---
+++ package1/MultipleCatchExample.java
@@ -0,0 +1,25 @@
+package package1;
+
+// multiple catch for a try is OK
+// if java is not able to find the appropriate catch then it will move ahead and check another catch
+// if that is desirable then it will throw that exception
+public class MultipleCatchExample {
+    public static void main(String[] args) {
+        try {
+            System.out.println(4/0);
+            System.out.println("This will never be printed if exception occurs");
+        }
+        catch (NullPointerException e){
+            System.out.println("Exception: "+e.getMessage());
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Exception: "+e.getMessage());
+
+        }
+        catch (ArithmeticException e){
+            System.out.println("Exception: "+e.getMessage());
+            // here java will check above two exception
+            // both are not right then this ArithmeticException will be executed.
+        }
+    }
+}
