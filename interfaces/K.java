fc9d427f9e0ec1293dc88e5e686b9e6bdba0c086
Rohuu <rohuu99022@gmail.com>
2023-03-04 17:00:37 +0530

    nested interfaces

---
+++ interfaces/K.java
@@ -0,0 +1,25 @@
+package interfaces;
+
+public interface K { //top interface can be public or default one
+    public interface NestedInterface{ // but nested interface can be anything
+        boolean isOdd(int num);
+    }
+}
+
+class L implements K.NestedInterface {
+
+    @Override
+    public boolean isOdd(int num) {
+        if(num%2==0){
+            return false;
+        }
+        return true;
+    }
+}
+
+class MainOfNested{
+    public static void main(String[] args) {
+        L obj=new L();
+        System.out.println(obj.isOdd(5));
+    }
+}
