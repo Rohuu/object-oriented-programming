71ccf430212f4be5232b166c0373286ecc40cda9
Rohuu <rohuu99022@gmail.com>
2023-03-04 13:21:22 +0530

    what is interface and diffs b/w abstract classes and interfaces.

---
+++ package1/Engine.java
@@ -0,0 +1,18 @@
+package package1;
+// interface: interfaces contains abstract functions(functions without body)
+// its like abstract class only ... functions are public and abstract..... and variables are static and final by default in interfaces.
+// interfaces mostly specifies like what the class is doing not how it is doing it.
+// how it is doing is specifies by the child classes which implements the interface
+// abstract class can have some information but interface can't
+// abstract classes can have any type of methods default,static,abstract methods
+// in interface variables are final
+// coz we have no constructor in interface
+// if no constructor then how will we initialise values, that's why they are final
+// but
+// in interface, inside methods variables can be final or non-final.
+// abstract classes can be extended by using extends keyword
+// interfaces can be implemented by using implements keyword
+// abstract classes can't be multiple inherited, but we can implement more than one interfaces
+public interface Engine {
+
+}
