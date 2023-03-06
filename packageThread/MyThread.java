aa4bc8c8dcb93d3fa595e8c78e75f60d55c27bd6
Rohuu <rohuu99022@gmail.com>
2023-03-06 19:37:57 +0530

    creating subclass of java Thread class

---
+++ packageThread/MyThread.java
@@ -0,0 +1,13 @@
+package packageThread;
+
+public class MyThread extends Thread{
+    public void run(){
+        System.out.println("MyThread running");
+        System.out.println("MyThread finished");
+    }
+
+    public static void main(String[] args) {
+        MyThread myThread=new MyThread();
+        myThread.start();
+    }
+}
