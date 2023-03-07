1147d4810fa08a68be16cd45a85c7e1043a96774
Rohuu <rohuu99022@gmail.com>
2023-03-07 21:01:37 +0530

    shallow copy

--- package1/MainOfWatch.java
+++ package1/MainOfWatch.java
@@ -1,10 +1,24 @@
 package package1;
 
+import java.util.Arrays;
+
 public class MainOfWatch {
     public static void main(String[] args) throws CloneNotSupportedException {
-        Watch casio=new Watch(5100,"Olive Green");
+        Watch casio=new Watch(5100,"Olive Green" , new int[]{1,2,3,4});
         Watch fakeWatch=(Watch)casio.clone();
-        System.out.println(fakeWatch.price);
-        System.out.println(fakeWatch.color);
+        System.out.println(Arrays.toString(casio.arr));   // [1, 2, 3, 4]
+        System.out.println(Arrays.toString(fakeWatch.arr));  // [1, 2, 3, 4]
+        // now if we make any changes in fakeWatch
+        fakeWatch.arr[0]=5;
+        System.out.println(Arrays.toString(casio.arr));   // [5, 2, 3, 4]
+        System.out.println(Arrays.toString(fakeWatch.arr));   // [5, 2, 3, 4]
+        // you see... we changed value in fake object
+        // but the original object also got changed
+        // this is called shallow copy
+        // means when we will make copy of objects then primitive types will be copied as it is
+        // but non primitives will be referring to same variables
+        // if we don't want that then we make deep copy of object
+        // where every data-type from one object is copied to another object
+        // thus change in the copied object will not impact the original object
     }
 }
--- package1/Watch.java
+++ package1/Watch.java
@@ -3,10 +3,12 @@ package package1;
 public class Watch implements Cloneable{
     int price;
     String color;
+    int[] arr;
 
-    public Watch(int price, String color) {
+    public Watch(int price, String color, int[] arr) {
         this.price = price;
         this.color = color;
+        this.arr=arr;
     }
 
     public Object clone() throws CloneNotSupportedException{
