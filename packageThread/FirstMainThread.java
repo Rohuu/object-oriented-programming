ea8920ace20c9c6f3229cde2a414089ff06fb07d
Rohuu <rohuu99022@gmail.com>
2023-03-08 12:58:37 +0530

    sleep thread

--- packageThread/FirstMainThread.java
+++ packageThread/FirstMainThread.java
@@ -8,5 +8,11 @@ public class FirstMainThread {
         // set name
         firstThread.setName("First main thread");
         System.out.println(firstThread.getName());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Printing this after sleep-time.... ");
     }
 }
