6f940d9aaa727dc917bf9ff24b74ad42d10b1daa
Rohuu <rohuu99022@gmail.com>
2023-03-08 15:42:22 +0530

    another Thread.sleep example

--- packageThread/FirstMainThread.java
+++ packageThread/FirstMainThread.java
@@ -2,17 +2,42 @@ package packageThread;
 
 public class FirstMainThread {
     public static void main(String[] args) {
-        Thread firstThread=Thread.currentThread();
-        System.out.println("Current Thread: "+firstThread.getName());
+//        Thread firstThread=Thread.currentThread();
+//        System.out.println("Current Thread: "+firstThread.getName());
+//
+//        // set name
+//        firstThread.setName("First main thread");
+//        System.out.println(firstThread.getName());
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println("Printing this after sleep-time.... ");
 
-        // set name
-        firstThread.setName("First main thread");
-        System.out.println(firstThread.getName());
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for (int i = 0; i < 3; i++) {
+            System.out.println("This");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        System.out.println("Printing this after sleep-time.... ");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("That");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            System.out.println("End");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
