1c041ea2f78840949df5e30134f94de4d834b4de
Rohuu <rohuu99022@gmail.com>
2023-03-08 12:43:25 +0530

    set name of thread

--- packageThread/FirstMainThread.java
+++ packageThread/FirstMainThread.java
@@ -4,5 +4,9 @@ public class FirstMainThread {
     public static void main(String[] args) {
         Thread firstThread=Thread.currentThread();
         System.out.println("Current Thread: "+firstThread.getName());
+
+        // set name
+        firstThread.setName("First main thread");
+        System.out.println(firstThread.getName());
     }
 }
