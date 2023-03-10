fcfc50686caf34699817246842c9ba733e1c92c6
Rohuu <rohuu99022@gmail.com>
2023-03-10 19:55:44 +0530

    stopping a thread abruptly using thread.join() & System.exit()

--- packageThread/CustomThreadPauseResume.java
+++ packageThread/CustomThreadPauseResume.java
@@ -9,11 +9,14 @@ public class CustomThreadPauseResume {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        if(customThread.isAlive()){
-            customThread.setShouldExit(true);
-            customThread.join();
-        }
+        // but if we comment out the lower if part & use join instead then it will abruptly end the thread
+//        if(customThread.isAlive()){
+//            customThread.setShouldExit(true);
+//            customThread.join();
+//        }
         // now thread is completed properly and while running, it will show thread is completed at end even if we have stopped it in between, it will not be stopped abruptly
+        // using thread.join, now it will abruptly end the thread
+        customThread.join(2000);
         System.exit(0);
         // exited instantly without letting customThread start coz of System.exit();
     }
