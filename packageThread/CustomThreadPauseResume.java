7588f4bcb2161f57753bc953dc24d86904b98144
Rohuu <rohuu99022@gmail.com>
2023-03-10 19:35:07 +0530

    thread.join() explained

--- packageThread/CustomThreadPauseResume.java
+++ packageThread/CustomThreadPauseResume.java
@@ -4,7 +4,14 @@ public class CustomThreadPauseResume {
     public static void main(String[] args) {
         CustomThread customThread=new CustomThread();
         customThread.start();
+        try {
+            customThread.join(2000);
+            // now we this will let the process happen for the given time and if that doesn't get executed in that time period then it will kill the process
+            // But first it will wait for given time
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.exit(0);
-        // exited instantly without letting customThread start coz of
+        // exited instantly without letting customThread start coz of System.exit();
     }
 }
