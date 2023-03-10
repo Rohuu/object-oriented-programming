debadd1a81799d4ca18da0993871145a9e021b74
Rohuu <rohuu99022@gmail.com>
2023-03-10 19:49:23 +0530

    safely stopping a thread

--- packageThread/CustomThread.java
+++ packageThread/CustomThread.java
@@ -1,9 +1,19 @@
 package packageThread;
 
-public class CustomThread extends Thread{
+public class CustomThread extends Thread {
+
+    private boolean shouldExit = false;
+
+    public void setShouldExit(boolean shouldExit) {
+        this.shouldExit = shouldExit;
+    }
+
     @Override
     public void run() {
-        for (int i = 0; i < 30 ; i++) {
+        for (int i = 0; i < 30; i++) {
+            if (shouldExit) {
+                break;
+            }
             System.out.println("Thread is running");
             try {
                 Thread.sleep(200);
--- packageThread/CustomThreadPauseResume.java
+++ packageThread/CustomThreadPauseResume.java
@@ -1,16 +1,19 @@
 package packageThread;
 
 public class CustomThreadPauseResume {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         CustomThread customThread=new CustomThread();
         customThread.start();
         try {
-            customThread.join(2000);
-            // now we this will let the process happen for the given time and if that doesn't get executed in that time period then it will kill the process
-            // But first it will wait for given time
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        if(customThread.isAlive()){
+            customThread.setShouldExit(true);
+            customThread.join();
+        }
+        // now thread is completed properly and while running, it will show thread is completed at end even if we have stopped it in between, it will not be stopped abruptly
         System.exit(0);
         // exited instantly without letting customThread start coz of System.exit();
     }
