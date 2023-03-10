56c1732f570cc2d7ddcef0e4d663a68c4a678d9a
Rohuu <rohuu99022@gmail.com>
2023-03-10 19:31:00 +0530

    join and System.exit

--- packageThread/ThreadStatus.java
+++ packageThread/ThreadStatus.java
@@ -30,5 +30,20 @@ public class ThreadStatus implements Runnable {
             }
         }
         System.out.println("Thread is completed");
+        try {
+            thread.join(2000);
+            // if we will add time in parameter then this will wait for that particular time so that the earlier thread can be completed
+            // if it is not completed within that time then this thread.join(); will kill the process.
+            // now the called thread will be completed first
+            // then the process will be killed
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Joining completed");
+        System.exit(0);
+        // this will create a high priority thread and program will be exited, this will kill the above running thread
+
+        // if we want this thread, to complete the running thread first, then kill the process
+        // then we have to use Thread.join(); before killing the process
     }
 }
