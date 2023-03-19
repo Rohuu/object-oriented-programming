81fc8b530db8549f529ee966a3b71a07ad8f8c65
Rohuu <rohuu99022@gmail.com>
2023-03-19 19:29:04 +0530

    inserting values to the start  and displaying , displaying reverse of a doubly LL

---
+++ LinkedList/MainDLL.java
@@ -0,0 +1,14 @@
+package LinkedList;
+
+public class MainDLL {
+    public static void main(String[] args) {
+        DLL list=new DLL();
+        list.insertFirst(2);
+        list.insertFirst(4);
+        list.insertFirst(25);
+        list.insertFirst(32);
+        list.insertFirst(82);
+        list.insertFirst(20);
+        list.display();
+    }
+}
