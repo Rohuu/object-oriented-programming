a034e6b5de513eb4b2c0cb3b6af1a18396cb21b5
Rohuu <rohuu99022@gmail.com>
2023-03-27 22:57:22 +0530

    merge two sorted lists

--- LinkedList/LL.java
+++ LinkedList/LL.java
@@ -10,120 +10,146 @@ public class LL {
         this.size = 0;
     }
 
-    public void insertFirst(int value){
-        Node node= new Node(value);
-        node.next=head;
-        head=node;
+    public void insertFirst(int value) {
+        Node node = new Node(value);
+        node.next = head;
+        head = node;
 
-        if(tail==null){ // means this is the first item added or there is only one item
+        if (tail == null) { // means this is the first item added or there is only one item
             // in that case head,tail,node all will be same
-            tail=head;
+            tail = head;
         }
         size++;
     }
 
-    public void display(){
-        Node temp=head;
-         while (temp!=null){
-            System.out.print(temp.value+ " ");
-            temp=temp.next;
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " ");
+            temp = temp.next;
         }
         System.out.println("END");
     }
 
     public void insertLast(int value) {
-        if(tail==null){
+        if (tail == null) {
             insertFirst(value);
             return;
         }
-        Node node=new Node(value);
-        tail.next=node;
-        tail=node;
+        Node node = new Node(value);
+        tail.next = node;
+        tail = node;
         size++;
     }
 
-    public void insert(int value, int index){
-        if(index==0){
+    public void insert(int value, int index) {
+        if (index == 0) {
             insertFirst(value);
             return;
         }
-        if(index==size){
+        if (index == size) {
             insertLast(value);
         }
-        Node temp=head;
+        Node temp = head;
         for (int i = 1; i < index; i++) {
-            temp=temp.next;
+            temp = temp.next;
         }
-        Node node=new Node(value,temp.next);
-        temp.next=node;
+        Node node = new Node(value, temp.next);
+        temp.next = node;
         size++;
     }
 
-    public int deleteFirst(){
-        int val=head.value;
-        head=head.next;
-        if(head==null){
-            tail=null;
+    public int deleteFirst() {
+        int val = head.value;
+        head = head.next;
+        if (head == null) {
+            tail = null;
         }
         size--;
         return val;
     }
 
-    public Node getIndex(int index){
-        Node node=head;
+    public Node getIndex(int index) {
+        Node node = head;
         for (int i = 0; i < index; i++) {
-            node=node.next;
+            node = node.next;
         }
         return node;
     }
 
-    public Node getNode(int value){
-        Node node=head;
-        while(node!=null){
-            if(node.value==value){
+    public Node getNode(int value) {
+        Node node = head;
+        while (node != null) {
+            if (node.value == value) {
                 return node;
             }
-            node=node.next;
+            node = node.next;
         }
         return null;
     }
 
-    public int deleteLastElement(){
-        if(size<=1){
+    public int deleteLastElement() {
+        if (size <= 1) {
             return deleteFirst();
         }
 
-        Node secondLast=getIndex(size-2);
-        int val=tail.value;
-        tail=secondLast;
-        tail.next=null;
+        Node secondLast = getIndex(size - 2);
+        int val = tail.value;
+        tail = secondLast;
+        tail.next = null;
         return val;
     }
 
-    public int delete(int index){
-        if(index==0){
+    public int delete(int index) {
+        if (index == 0) {
             return deleteFirst();
         }
-        if(index==size-1){
+        if (index == size - 1) {
             return deleteLastElement();
         }
-        Node prev=getIndex(index-1);
-        int val=prev.next.value;
-        prev.next=prev.next.next;
+        Node prev = getIndex(index - 1);
+        int val = prev.next.value;
+        prev.next = prev.next.next;
         return val;
     }
 
-    private static class Node{
-        int value;
-        Node next;
-
-        public Node(int value) {
-            this.value = value;
+    // merge
+    public static LL merge(LL first, LL second) {
+        LL ans = new LL();
+        Node head1 = first.head;
+        Node head2 = second.head;
+        while (head1 != null && head2 != null) {
+            if (head1.value > head2.value) {
+                ans.insertLast(head2.value);
+                head2 = head2.next;
+            } else {
+                ans.insertLast(head1.value);
+                head1 = head1.next;
+            }
         }
+        while (head1 != null) {
+            ans.insertLast(head1.value);
+            head1 = head1.next;
+        }
+        while (head2 != null) {
+            ans.insertLast(head2.value);
+            head2 = head2.next;
+        }
+        return ans;
+    }
 
-        public Node(int value, Node next) {
-            this.value = value;
-            this.next = next;
+        private static class Node {
+            int value;
+            Node next;
+
+            public Node(int value) {
+                this.value = value;
+            }
+
+            public Node(int value, Node next) {
+                this.value = value;
+                this.next = next;
+            }
         }
     }
-}
+
--- LinkedList/MainLL.java
+++ LinkedList/MainLL.java
@@ -1,25 +1,17 @@
 package LinkedList;
 
-import org.w3c.dom.Node;
-
 public class MainLL {
     public static void main(String[] args) {
-        LL list= new LL();
-        list.insertFirst(3);
-        list.insertFirst(2);
-        list.insertFirst(8);
-        list.insertFirst(17);
-        list. insertLast(4);
-        list.insertLast(99);
-        list.insertLast(999);
-        list.insert(100,3);
-        list.display();
-        int deletedNumber=list.deleteFirst();
-        System.out.println("Deleted number is: "+deletedNumber);
-        list.display();
-        System.out.println("Deleted number is: "+list.deleteLastElement());
-        list.display();
-        list.delete(2);
-        list.display();
+        LL first= new LL();
+        LL second= new LL();
+        first.insertLast(1);
+        first.insertLast(3);
+        first.insertLast(5);
+        second.insertLast(1);
+        second.insertLast(2);
+        second.insertLast(9);
+        second.insertLast(14);
+        LL ans=LL.merge(first,second);
+        ans.display();
     }
 }
