c79060af85d63045b39cec29afc9e9ec9a0cdf3a
Rohuu <rohuu99022@gmail.com>
2023-03-31 23:56:29 +0530

    Find length of cycle in a Linked List

--- LinkedList/CycleQuestions.java
+++ LinkedList/CycleQuestions.java
@@ -18,6 +18,27 @@ public class CycleQuestions {
         }
         return false;
     }
+
+    // Find length of cycle in a Linked List
+    public int lengthCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next.next;
+            if (slow == fast) {
+                // cycle found, now calculate length
+                ListNode temp=slow.next;
+                int length=1;
+                while(temp!=fast){
+                    temp=temp.next;
+                    length=length+1;
+                }
+                return length;
+            }
+        }
+        return 0;
+    }
 }
 
 class ListNode {
