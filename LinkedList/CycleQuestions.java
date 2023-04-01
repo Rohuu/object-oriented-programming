9b9408171d4d3d72e289b59ec0c4b398d16da19d
Rohuu <rohuu99022@gmail.com>
2023-04-01 23:51:52 +0530

    Happy Number

--- LinkedList/CycleQuestions.java
+++ LinkedList/CycleQuestions.java
@@ -39,6 +39,33 @@ public class CycleQuestions {
         }
         return 0;
     }
+
+    // happy number
+    // https://leetcode.com/problems/happy-number/
+
+    public boolean isHappy(int n) {
+        int slow=n;
+        int fast=n;
+        do {
+            slow=findSquare(slow);
+            fast=findSquare(findSquare(fast));
+        }
+        while (slow!=fast);
+        if(slow==1){
+            return true;
+        }
+        return false;
+    }
+
+    private int findSquare(int number){
+        int ans=0;
+        while(number>0){
+            int rem=number%10;
+            ans=ans+rem*rem;
+            number=number/10;
+        }
+        return ans;
+    }
 }
 
 class ListNode {
