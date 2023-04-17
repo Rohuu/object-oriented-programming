2ad42ee50b8829be8586ad677ebb9b1cd51da27c
Rohuu <rohuu99022@gmail.com>
2023-04-17 12:53:42 +0530

    reverse a linked list

--- LinkedList/CycleQuestions.java
+++ LinkedList/CycleQuestions.java
@@ -124,6 +124,20 @@ public class CycleQuestions {
         }
         return slow;
     }
+
+    // reverse a linkedList
+    // https://leetcode.com/problems/reverse-linked-list/description/
+    public ListNode reverseList(ListNode head) {
+        ListNode curr=head;
+        ListNode prev=null;
+        while(curr!=null){
+            ListNode temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+    }
 }
 
 class ListNode {
